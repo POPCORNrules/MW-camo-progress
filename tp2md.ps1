0112b71d293db1626ffaeba583c51a229313d1f0
Tony Arredondo <tony.arredondo5@gmail.com>
2020-08-11 10:47:07 -0500

    fix formatting

--- tp2md.ps1
+++ tp2md.ps1
@@ -1,9 +1,9 @@
 $taskpaper = get-content "./Gold Camo.taskpaper"
 $taskpaper = $taskpaper | Select-Object -Skip 1
 $readme = Get-Content "./README.md"
-$readme = $readme | Select-Object -First 4
+$readme = $readme | Select-Object -First 5
 foreach ($item in $taskpaper) {
-        $readme += $item -replace '(^\W+)?(.*):','$1## $2' -replace '(\W+)?(-\W)(.*)','$1- [ ] $3' -replace '(\W+)?(-\W\[\W\]\W)(.*)(\W@done)','$1- [x] $3'
+        $readme += $item -replace '(^\W+)?(.*):','## $2' -replace '(\W+)?(-\W)(.*)','- [ ] $3' -replace '(\W+)?(-\W\[\W\]\W)(.*)(\W@done)','- [x] $3'
 }
 
 $readme | Out-File "./README.md"
