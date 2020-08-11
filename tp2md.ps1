d41eea6fa0f8c3b85a459473701a17cef04f7693
Tony Arredondo <tony.arredondo5@gmail.com>
2020-08-11 10:48:53 -0500

    update formatting

--- tp2md.ps1
+++ tp2md.ps1
@@ -3,7 +3,7 @@ $taskpaper = $taskpaper | Select-Object -Skip 1
 $readme = Get-Content "./README.md"
 $readme = $readme | Select-Object -First 5
 foreach ($item in $taskpaper) {
-        $readme += $item -replace '(^\W+)?(.*):','## $2' -replace '(\W+)?(-\W)(.*)','- [ ] $3' -replace '(\W+)?(-\W\[\W\]\W)(.*)(\W@done)','- [x] $3'
+        $readme += $item -replace '(^\W+)?(.*):','### $2' -replace '(\W+)?(-\W)(.*)','- [ ] $3' -replace '(\W+)?(-\W\[\W\]\W)(.*)(\W@done)','- [x] $3'
 }
 
 $readme | Out-File "./README.md"
